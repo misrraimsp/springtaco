5bb44858966da3b7433651a1defc490a0ab2839b
misrra <misrraimsp@gmail.com>
2019-12-18 09:11:44 +0000

    project structure with spring boot

---
+++ src/main/java/misrraimsp/sia/springtaco/SpringtacoApplication.java
@@ -0,0 +1,13 @@
+package misrraimsp.sia.springtaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtacoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/misrraimsp/sia/springtaco/SpringtacoApplicationTests.java
@@ -0,0 +1,13 @@
+package misrraimsp.sia.springtaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtacoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
