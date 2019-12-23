48262e9162698b3c3537c2a69af0271d580d376e
misrra <misrraimsp@gmail.com>
2019-12-23 13:24:36 +0000

    Added customized Spring Security authentication with JPA. Secured web requests. Created custom login.html Added logout functionality

---
+++ src/main/java/misrraimsp/sia/springtaco/services/UserRepositoryUserDetailsService.java
@@ -0,0 +1,29 @@
+package misrraimsp.sia.springtaco.services;
+
+import misrraimsp.sia.springtaco.data.UserRepository;
+import misrraimsp.sia.springtaco.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserRepositoryUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            return user;
+        }
+        throw new UsernameNotFoundException("User '" + username + "' not found");
+    }
+}
